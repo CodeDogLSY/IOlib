301bc6b3f62224ca11bfc8027834ec206666598c
lsy <liushiyang2016@gmail.com>
2018-02-02 21:17:41 +0800

    添加rxjava 依赖

--- app/src/main/java/com/lib/lsy/sample/MainActivity.java
+++ app/src/main/java/com/lib/lsy/sample/MainActivity.java
@@ -1,13 +1,23 @@
-package com.lib.lsy.iolib;
+package com.lib.lsy.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import com.lib.lsy.iolib.R;
+
+
+/**
+ * @author lsy
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
     }
 }
---
+++ app/src/main/java/com/lib/lsy/sample/basesample/DemoApp.java
@@ -0,0 +1,19 @@
+package com.lib.lsy.sample.basesample;
+
+import com.lib.lsy.iolib.base.IOApplication;
+
+/**
+ * Created on 2018/2/2.
+ * 功能描述：
+ * 版本：
+ * 待处理部分：
+ *
+ * @author lsy
+ */
+
+public class DemoApp extends IOApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
