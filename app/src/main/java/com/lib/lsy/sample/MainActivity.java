5d8d19a4b50d22ddac60e37d87f65ebd6da8951d
lsy <liushiyang2016@gmail.com>
2018-02-05 15:27:47 +0800

    开启lambdas

--- app/src/main/java/com/lib/lsy/sample/MainActivity.java
+++ app/src/main/java/com/lib/lsy/sample/MainActivity.java
@@ -1,15 +1,17 @@
 package com.lib.lsy.sample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.lib.lsy.iolib.R;
+import com.lib.lsy.sample.demo.RxActivity;
 
 
 /**
  * @author lsy
- * 主页入口
+ *         主页入口
  */
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void toRx(View view) {
+        Intent intent = new Intent(MainActivity.this, RxActivity.class);
+        startActivity(intent);
     }
 
     public void toNet(View view) {
--- app/src/main/java/com/lib/lsy/sample/demo/RxActivity.java
+++ app/src/main/java/com/lib/lsy/sample/demo/RxActivity.java
@@ -2,8 +2,12 @@ package com.lib.lsy.sample.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.lib.lsy.iolib.R;
+
+import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * 展示rx示例
@@ -14,5 +18,71 @@ public class RxActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx);
+    }
+
+    public void sample(View view) {
+        Flowable.just("Hello world").subscribe(System.out::println);
+    }
+
+    public void backGround(View view) {
+        Flowable.fromCallable(() -> {
+            //  imitate expensive computation
+            Thread.sleep(1000);
+            return "Done";
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.single())
+                .subscribe(System.out::println, Throwable::printStackTrace);
+        // <--- wait for the flow to finish
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void backGround2(View view) {
+        Flowable<String> source = Flowable.fromCallable(() -> {
+            Thread.sleep(1000); //  imitate expensive computation
+            return "Done";
+        });
+
+        Flowable<String> runBackground = source.subscribeOn(Schedulers.io());
+
+        Flowable<String> showForeground = runBackground.observeOn(Schedulers.single());
+
+        showForeground.subscribe(System.out::println, Throwable::printStackTrace);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void backGround3(View view) {
+        Flowable.range(1, 10)
+                .observeOn(Schedulers.computation())
+                .map(v -> v * v)
+                .blockingSubscribe(System.out::println);
+    }
+
+    public void backGround4(View view) {
+        Flowable.range(1, 10)
+                .flatMap(v ->
+                        Flowable.just(v)
+                                .subscribeOn(Schedulers.computation())
+                                .map(w -> w * w)
+                )
+                .blockingSubscribe(System.out::println);
+    }
+
+    public void backGround5(View view) {
+        Flowable.range(1, 10)
+                .parallel()
+                .runOn(Schedulers.computation())
+                .map(v -> v * v)
+                .sequential()
+                .blockingSubscribe(System.out::println);
     }
 }
