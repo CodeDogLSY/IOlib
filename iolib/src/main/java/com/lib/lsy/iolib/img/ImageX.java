c7497dc77e3fa942c036c4bce8179e6c027fd6bd
lsy <liushiyang2016@gmail.com>
2018-02-06 22:29:48 +0800

    修改imagex依赖jar包中的R文件的问题

--- iolib/src/main/java/com/lib/lsy/iolib/img/ImageX.java
+++ iolib/src/main/java/com/lib/lsy/iolib/img/ImageX.java
@@ -23,17 +23,17 @@ import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOption
 
 public class ImageX {
 
-    /*
-    普通加载，不带任何效果
+    /**
+     * 普通加载，不带任何效果
      */
     public static void loadNormal(Context context, String url, ImageView img) {
         GlideApp.with(context)
                 .load(url).into(img);
     }
 
-    /*
-    普通加载，加载失败的显示
-    */
+    /**
+     * 普通加载，加载失败的显示
+     */
     public static void loadNormalNull(Context context, String url, int resource, ImageView img) {
         GlideApp.with(context)
                 .load(url)
@@ -41,28 +41,29 @@ public class ImageX {
                 .into(img);
     }
 
-    /*
-    普通加载，处理了占位（颜色），加载失败（通用），淡入淡出
+    /**
+     * 普通加载，处理了占位（颜色），加载失败（通用），淡入淡出
      */
-    public static void loadNormalShow(Context context, String url, ImageView img) {
+    public static void loadNormalShow(Context context, String url,int color, ImageView img) {
         GlideApp.with(context)
                 .load(url)
                 //加载中占位符，
                 .placeholder(new ColorDrawable(ContextCompat.getColor(IOApplication.getInstance(), R.color.gray_hard)))
                 //                加载错误
-//                .error(R.mipmap.ic_launcher)
-                .error(R.color.gray_hard)
+                //                .error(R.mipmap.ic_launcher)
+//                .error(R.color.gray_hard)
+                .error(color)
                 //交叉淡入变换
                 .transition(withCrossFade())
                 .into(img);
     }
 
 
-    /*
-   加载gif
-   R.mipmap.loading
+    /**
+     * 加载gif
+     * R.mipmap.loading
      */
-    public static void loadGif(Context context, int intid, ImageView img) {
+    public static void loadGif(Context context, String imgurl, int color, ImageView img) {
         //        GlideApp.with(context)
         //                .asGif()
         //                .load(intid)
@@ -72,18 +73,20 @@ public class ImageX {
                 //                .load("http://ww2.sinaimg.cn/large/005ZwB0Dgw1exzaqyqg65g30an04thdz.gif")
                 //                .load("http://img.youbiaoqing.com/u/1b20a885f4dfd524efb99ef0c4c1f53f.gif")
                 //                .load("http://p3.pstatp.com/large/19f30002ad7da16b3d2a")
-                .load("http://p9.pstatp.com/large/19ec0002c813434615ae")
-                .placeholder(new ColorDrawable(ContextCompat.getColor(IOApplication.getInstance(), R.color.gray_hard)))
+                //                .load("http://p9.pstatp.com/large/19ec0002c813434615ae")
+                .load(imgurl)
+                //                .placeholder(new ColorDrawable(ContextCompat.getColor(IOApplication.getInstance(), R.color.gray_hard)))
+                .placeholder(new ColorDrawable(ContextCompat.getColor(IOApplication.getInstance(), color)))
                 .transition(withCrossFade())
                 //                .transforms(new CenterCrop(), new GlideRoundTransform(context, 50))
                 .into(img);
 
     }
 
-    /*
-加载圆角图片
-app:layout_auto_basewidth="height" 迫使宽高正常，避免变形
- */
+    /**
+     * 加载圆角图片
+     * app:layout_auto_basewidth="height" 迫使宽高正常，避免变形
+     */
     public static void loadRound(Context context, String url, ImageView img, int round) {
         GlideApp.with(context)
                 .load(url)
@@ -91,9 +94,9 @@ app:layout_auto_basewidth="height" 迫使宽高正常，避免变形
                 .into(img);
     }
 
-    /*
-加载圆形图片
- */
+    /**
+     * 加载圆形图片
+     */
     public static void loadCircle(Context context, String url, ImageView img) {
         GlideApp.with(context)
                 .load(url)
